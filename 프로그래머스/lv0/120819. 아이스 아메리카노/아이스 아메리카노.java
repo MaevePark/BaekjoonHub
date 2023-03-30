25fa48d9b79cf3e7ced9bf7ff86960873f8895ec
MinJiPark <111377620+MaevePark@users.noreply.github.com>
2023-03-30 21:24:28 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2]; // 배열 선언
+
+        answer[0] = money/5500;
+        answer[1] = money%5500;
+        
+        
+        return answer;
+    }
+}
