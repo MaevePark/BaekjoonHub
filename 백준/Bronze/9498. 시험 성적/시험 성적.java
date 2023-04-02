94ccde432e31d9543366cbdc8218b34be6dc13b6
MinJiPark <111377620+MaevePark@users.noreply.github.com>
2023-04-02 21:20:42 +0900

    Update 시험 성적.java

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -7,6 +7,6 @@ public class Main {
 		int A = in.nextInt();
 		in.close();
 		
-		System.out.print((A>=90)?"A": (A>=80)? "B": (A>=70)? "C": (A>=60)? "D": "F");
+		System.out.print((A>=90)?"A": (A>=80)? "B": (A>=70)? "C": (A>=60)? "D": "F"); // 기초적인 방법으로, 삼항연산자를 중첩하여 작성
 	}
 }
