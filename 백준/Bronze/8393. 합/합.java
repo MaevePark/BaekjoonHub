4ada98dde587c1fe1f099d61689747e0f0677807
MinJiPark <111377620+MaevePark@users.noreply.github.com>
2023-04-18 18:33:12 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Main {
+
+	public static void main(String[] args) {
+        
+		int a; // for문의 반복할 변수 선언
+		int sum = 0; // 합계 값 변수 선언
+        
+        Scanner sc = new Scanner(System.in); // 값 입력
+        a = sc.nextInt();
+     
+		for (int i = 1; i <= a; i++) { // i 부터 a 까지의 합
+			sum += i;
+		}
+        System.out.println(sum); // 합계 값 결과 출력
+        
+        sc.close();
+	}
+}
