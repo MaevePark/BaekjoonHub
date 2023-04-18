4915ee6c1dd459ab39f84e55f23f14195a2c0b1c
MinJiPark <111377620+MaevePark@users.noreply.github.com>
2023-04-18 18:35:34 +0900

    Update 합.java

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -4,15 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
         
-		int a; // for문의 반복할 변수 선언
-		int sum = 0; // 합계 값 변수 선언
+	int a; // for문의 반복할 변수 선언
+	int sum = 0; // 합계 값 변수 선언
         
         Scanner sc = new Scanner(System.in); // 값 입력
         a = sc.nextInt();
      
-		for (int i = 1; i <= a; i++) { // i 부터 a 까지의 합
-			sum += i;
-		}
+	for (int i = 1; i <= a; i++) { // i 부터 a 까지의 합
+		sum += i;
+	}
         System.out.println(sum); // 합계 값 결과 출력
         
         sc.close();
