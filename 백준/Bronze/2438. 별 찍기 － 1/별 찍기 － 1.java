3b8ff4eeae051180addbeb6302a633b638e0abed
MinJiPark <111377620+MaevePark@users.noreply.github.com>
2023-04-19 16:59:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18148 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 1; i <= n; ++i) { // 줄 반복
+            for (int j = 1; j <= i; ++j) { // 별 반복 i의 증가수 만큼 별 갯수 증가
+                System.out.print("*"); // 별 찍기
+            }
+            System.out.println(); // 별 찍기후에 줄 바꿈
+        }
+    }
+}
